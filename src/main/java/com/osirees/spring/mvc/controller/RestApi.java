f88bb66ff7e34cc904642054b633d216e866ff19
Deepak Panickar <deepak.panickar@gmail.com>
2022-03-25 08:59:43 +0530

    Added json response to the request

--- src/main/java/com/osirees/spring/mvc/controller/RestApi.java
+++ src/main/java/com/osirees/spring/mvc/controller/RestApi.java
@@ -1,15 +1,18 @@
 package com.osirees.spring.mvc.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.osirees.spring.mvc.model.Profile;
 
 @RestController
 public class RestApi {
 
 	@ResponseBody
-	@GetMapping(value = "/api", produces="application/json")
-	public String testApi() {
-		return "Success-6";
+	@GetMapping(value = "/api", produces=MediaType.APPLICATION_JSON_VALUE)
+	public Profile testApi() {
+		return new Profile("Deepak", "Mr", "Mumbai");
 	}
 }
